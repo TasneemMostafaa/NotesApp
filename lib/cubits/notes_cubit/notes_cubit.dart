91f6226072d2f9f83118849ae565ed9813708d52
Tasneem Mostafa <tasneemmostafa207@gmail.com>
2026-09-09 12:58:50 +0300

    display notes #1

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -8,15 +8,12 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
-
+  List<NoteModel> ?notes;
   fetchAll(){
-    try{
+    
     var notesBox = Hive.box<NoteModel>(kNotesBox);
-    List <NoteModel> notes = notesBox.values.toList();
-    emit(NotesSuccess(notes));
-    } catch(e){
-      emit(NotesFailure(toString()));
-    }
+    notes = notesBox.values.toList();
+    
+    
   }
 }
--- lib/view/widgets/notes_list_view.dart
+++ lib/view/widgets/notes_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notesapp/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notesapp/models/note_model.dart';
 import 'package:notesapp/view/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -15,18 +17,26 @@ class NotesListView extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsetsGeometry.symmetric(vertical: 8),
-      child: ListView.builder(
-        padding: EdgeInsetsGeometry.zero,
-        itemBuilder: (context, index) {
-          Color assignedColor = colorList[index % colorList.length];
-          return Padding(
-            padding: EdgeInsets.symmetric(vertical: 4),
-            child: NoteItem(noteColor: assignedColor),
-          );
-        },
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes ?? [];
+        
+        
+        return Padding(
+          padding: EdgeInsetsGeometry.symmetric(vertical: 8),
+          child: ListView.builder(
+            itemCount: notes.length,
+            padding: EdgeInsetsGeometry.zero,
+            itemBuilder: (context, index) {
+              Color assignedColor = colorList[index % colorList.length];
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: NoteItem(noteColor: assignedColor),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
